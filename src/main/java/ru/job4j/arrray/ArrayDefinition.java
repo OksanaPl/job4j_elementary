545a5513fc09561fd5560c5b4a33a2e11ed0108c
OksanaPl <ok965@yandex.ru>
2022-12-29 19:33:40 +0300

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/arrray/ArrayDefinition.java
+++ src/main/java/ru/job4j/arrray/ArrayDefinition.java
@@ -5,9 +5,18 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
-
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Ariana";
+        names[1] = "Alexander";
+        names[2] = "Philip";
+        names[3] = "Oksana";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
