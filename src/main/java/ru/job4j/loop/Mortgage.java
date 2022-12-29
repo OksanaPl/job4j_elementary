80be01b5856fdbea7c6b2266c7ecd20ea8a28ddb
OksanaPl <ok965@yandex.ru>
2022-12-29 15:38:17 +0300

    5.7. Ипотека [#156315 #375771]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -6,7 +6,7 @@ public class Mortgage {
         while (amount > 0) {
             amount += (amount * (percent / 100) - salary);
             year++;
-        }
+            }
         System.out.println(year);
         return year;
     }
