7303cd3d3e002abf2dc755402eafaa45a7303b2e
OksanaPl <ok965@yandex.ru>
2023-01-15 22:13:12 +0300

    6.9. String - это массив [#504884]

---
+++ src/main/java/ru/job4j/arrray/JavaNameValidator.java
@@ -0,0 +1,37 @@
+package ru.job4j.arrray;
+
+public class JavaNameValidator {
+
+    public static boolean isNameValid(String name) {
+        if (name.isEmpty()) {
+            return false;
+        }
+        char[] chars = name.toCharArray();
+        if (isUpperLatinLetter(chars[0]) || isDigit(chars[0])) {
+            return false;
+        }
+        for (int i = 1; i < chars.length; i++) {
+            if (isDigit(chars[i]) || isSpecialSymbol(chars[i])
+                || isLowerLatinLetter(chars[i]) || isUpperLatinLetter(chars[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isSpecialSymbol(int code) {
+        return code == 36 || code == 95;
+    }
+
+    public static boolean isUpperLatinLetter(int code) {
+        return code >= 65 && code <= 90;
+    }
+
+    public static boolean isLowerLatinLetter(int code) {
+        return code >= 97 && code <= 122;
+    }
+
+    public static boolean isDigit(int code) {
+        return code >= 48 && code <= 57;
+    }
+}
