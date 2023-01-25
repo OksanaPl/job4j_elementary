59ded06bf02d30a42471d96d3a96c978603e8ccd
OksanaPl <ok965@yandex.ru>
2023-01-25 22:00:55 +0300

    1. Чтение кода и базовые ошибки [#40205]

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -8,17 +8,14 @@ public class Fit {
 
     public static double womanWeight(short height) {
         return (height - 110) * 1.15;
-
     }
 
     public static void main(String[] args) {
         short height = 187;
         double man = Fit.manWeight(height);
         System.out.println("Man 187 is " + man);
-
         height = 165;
         double woman = Fit.womanWeight(height);
         System.out.println("Woman 165 is " + woman);
-
     }
 }
--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -21,9 +21,7 @@ public class MathCalculator {
 
     public static void main(String[] args) {
         System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
-
         System.out.println("Сумма разности и деления равна: " + minusAndDivision(8, 4));
-
         System.out.println("Сумма всех операций: " + sumAll(10, 20, 8, 4));
     }
 }
